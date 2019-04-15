45c556954dfb1e20909a3f00b357ff7980ea5a5c
matusca96 <gomesloud@gmail.com>
2019-04-15 16:07:33 -0300

    Padronização.

--- webapp/controller/Motorista.controller.js
+++ webapp/controller/Motorista.controller.js
@@ -1,8 +1,11 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageBox",
-	"sap/ui/model/json/JSONModel"
-], function(Controller, MessageBox, JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"br/com/idxtecMotorista/services/Session"
+], function(Controller, MessageBox, JSONModel, Filter, FilterOperator, Session) {
 	"use strict";
 
 	return Controller.extend("br.com.idxtecMotorista.controller.Motorista", {
@@ -11,6 +14,29 @@ sap.ui.define([
 			
 			this.getOwnerComponent().setModel(oParamModel, "parametros"); 
 			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+			
+			this.getModel().attachMetadataLoaded(function(){
+				var oFilter = new Filter("Empresa", FilterOperator.EQ, Session.get("EMPRESA_ID"));
+				var oView = this.getView();
+				var oTable = oView.byId("tableMotorista");
+				var oColumn = oView.byId("columnNome");
+				
+				oTable.sort(oColumn);
+				oView.byId("tableMotorista").getBinding("rows").filter(oFilter, "Application");
+			});
+		},
+		
+		filtraMotorista: function(oEvent){
+			var sQuery = oEvent.getParameter("query");
+			var oFilter1 = new Filter("Empresa", FilterOperator.EQ, Session.get("EMPRESA_ID"));
+			var oFilter2 = new Filter("Nome", FilterOperator.Contains, sQuery);
+			
+			var aFilters = [
+				oFilter1,
+				oFilter2
+			];
+
+			this.getView().byId("tableMotorista").getBinding("rows").filter(aFilters, "Application");
 		},
 		
 		onRefresh: function(e){
@@ -78,6 +104,10 @@ sap.ui.define([
 					oTable.clearSelection();
 				}
 			});
+		},
+		
+		getModel: function(sModel) {
+			return this.getOwnerComponent().getModel(sModel);
 		}
 	});
 });
